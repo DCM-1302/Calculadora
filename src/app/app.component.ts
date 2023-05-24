d69b9ae8ef4d3a3cdc5c2d36a0e57f333d12767c
DCM-1302 <dcmonave@gmail.com>
2023-05-24 13:49:17 -0500

    Ver 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_test';
+  operacion="0"
+  Num=0
+  numerar(Num: number) {
+    this.operacion="";
+    if (this.operacion === "0"){
+      this.operacion= Num.toString();
+    } else {
+      this.operacion= this.operacion+Num.toString();
+    }
+    document.getElementById("display")!.innerHTML=this.operacion
+  }
 }
