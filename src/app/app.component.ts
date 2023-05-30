7b12d186772096aba0d20c3e153df55ae8753e20
DCM-1302 <dcmonave@gmail.com>
2023-05-30 18:15:38 -0500

    Ver 4.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -72,11 +72,18 @@ export class AppComponent {
     let a = this.primis;
     let b = parseFloat(this.operacion);
 
+    
     if(this.ultimis=== 'e'){
       this.resultado= a**b;
     }
     else if (this.ultimis === 'd'){
-      this.resultado = a/b
+      if(b===0){
+        window.alert("No destruya al universo, por favor")
+        document.getElementById("display")!.innerHTML="ERROR"
+      } 
+      else {
+        this.resultado = a/b
+      }
     }
     else if (this.ultimis === 'm'){
       this.resultado = a*b
@@ -88,9 +95,15 @@ export class AppComponent {
       this.resultado = a-b
     }
 
-    this.mitadis = false;
-    this.primis = this.resultado
-    this.operacion = this.resultado.toString()
-    document.getElementById("display")!.innerHTML=this.operacion
+    if(Number.isNaN(this.resultado)){
+      window.alert("Hubo un error en la operación")
+      document.getElementById("display")!.innerHTML="ERROR"
+    }
+    else {
+      this.mitadis = false;
+      this.primis = this.resultado
+      this.operacion = this.resultado.toString()
+      document.getElementById("display")!.innerHTML=this.operacion
+    }
   }
 }
